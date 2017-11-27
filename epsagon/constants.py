f7bd2c69cfb23a95cc8a6a2373db2c493ccbaacd
Ran Rib <ran@epsagon.com>
2017-11-27 11:30:15 +0200

    Adding supported regions list for trace collection.

--- epsagon/constants.py
+++ epsagon/constants.py
@@ -5,7 +5,14 @@ import os
 __version_info__ = ('0', '1', '1')
 __version__ = '.'.join(__version_info__)
 
-DEFAULT_REGION = 'eu-central-1'
+TC_REGIONS = [
+    'us-east-1',
+]
+
+DEFAULT_REGION = 'us-east-1'
 REGION = os.environ.get('AWS_REGION', DEFAULT_REGION)
+if REGION not in TC_REGIONS:
+    REGION = DEFAULT_REGION
+
 TRACE_COLLECTOR_STREAM = 'tc.{0}'.format(REGION)
 COLD_START = True
