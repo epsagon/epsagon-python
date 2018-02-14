a5babc88e7cd42feda43d748f5dc677de37a7e01
gal bashan <gal@epsagon.com>
2018-02-14 10:05:30 +0200

    Adding validation before importing a module that the user has that module installed

--- epsagon/modules/__init__.py
+++ epsagon/modules/__init__.py
@@ -10,6 +10,8 @@ MODULES = {}
 for module_file in os.listdir(os.path.dirname(__file__)):
     if module_file == '__init__.py' or module_file[-3:] != '.py':
         continue
-
-    imported = __import__(module_file[:-3], locals(), globals())
-    MODULES[module_file[:-3]] = imported
+    try:
+        imported = __import__(module_file[:-3], locals(), globals())
+        MODULES[module_file[:-3]] = imported
+    except ImportError:
+        pass
