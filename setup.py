56be9d0c3cfb17e42103079e24db6000cc6857b2
Liad Goren <Liad@Epsagon.com>
2018-03-16 18:11:06 +0200

    Create Models repo

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 from pip.req import parse_requirements
 from pip.download import PipSession
 from setuptools import setup, find_packages
-import epsagon.constants
 
 install_reqs = parse_requirements('./requirements.txt', session=PipSession())
 reqs = [str(ir.req) for ir in install_reqs]
@@ -10,18 +9,13 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='epsagon',
-    version=epsagon.constants.__version__,
+    version=('0', '1', '1'),
     description='Epsagon instrumentation for serverless Architecture Performance Monitoring',
     author='Epsagon',
     author_email='support@epsagon.com',
     url='https://github.com/epsagon/epsagon',
     packages=find_packages(exclude=('tests', 'examples')),
     install_requires=reqs,
-    extras_require={
-        'dev': [
-            'flake8'
-        ]
-    },
     license="Apache License 2.0",
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
