12c85f9bc1f8d6cec34505cc556b004782c644d3
Erez Freiberger <enoodle@gmail.com>
2018-10-07 18:22:49 +0300

    add minimus package requirements for setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(
-    setup_requires=['pbr'],
+        setup_requires=['pbr>=4.3.0', 'setuptools>=40.4.3', 'twine>=1.12.1', 'wheel>=0.32.0', 'pkginfo>=1.4.2'],
     pbr=True
 )
