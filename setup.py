9a94781542f4f36f1080a5fc010dda4ca487b2c2
Ran Ribenzaft <ranrib@users.noreply.github.com>
2018-12-02 04:59:00 -0800

    fix(tox): add support for python 3.7 (#20)

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
     )
