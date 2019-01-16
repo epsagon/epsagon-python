034f75132253dd94e8f349aece68fa97774a388f
Gal Bashan <galbashan1@gmail.com>
2019-01-16 14:49:32 +0200

    fix(layer): publishing layer on new versions (#37)

---
+++ scripts/semantic_release.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git config --global user.name "semantic-release (via TravisCI)"
+git config --global user.email "semantic-release@travis"
+pip install --upgrade wheel setuptools twine pkginfo
+pip install python-semantic-release
+semantic-release publish
+
