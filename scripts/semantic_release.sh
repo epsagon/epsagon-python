36c7b4352a673103d1204bcb1e03d30b364fb449
semantic-release (via TravisCI) <semantic-release@travis>
2019-03-05 11:45:56 +0200

    feat(release): working on release

--- scripts/semantic_release.sh
+++ scripts/semantic_release.sh
@@ -3,5 +3,4 @@ git config --global user.name "semantic-release (via TravisCI)"
 git config --global user.email "semantic-release@travis"
 pip install --upgrade wheel setuptools twine pkginfo
 pip install python-semantic-release
-npm install -g semantic-release @semantic-release/exec
-node /usr/local/lib/node_modules/semantic-release/bin/semantic-release.js
+./node_modules/.bin/semantic-release
