c96e67b322eefbc8fcc8e2cfce616042c1b96cf4
gal bashan <gal@epsagon.com>
2019-03-05 13:44:15 +0200

    feat(release): fixing

--- scripts/semantic_release.sh
+++ scripts/semantic_release.sh
@@ -3,4 +3,5 @@ git config --global user.name "semantic-release (via TravisCI)"
 git config --global user.email "semantic-release@travis"
 pip install --upgrade wheel setuptools twine pkginfo
 pip install python-semantic-release
+npm install @semantic-release/exec semantic-release
 ./node_modules/.bin/semantic-release
