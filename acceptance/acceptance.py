b1e6e7b72120217dbf7b4088171d3c5f75832a86
Gal Bashan <galbashan1@gmail.com>
2019-03-05 16:23:51 +0200

    fix(acceptance): improving acceptance tests (#65)

---
+++ acceptance/acceptance.py
@@ -0,0 +1,60 @@
+import os
+import json
+import pytest
+import boto3
+
+SERVICE_PREFIX = 'epsagon-acceptance-{}-dev'.format(os.environ.get('TRAVIS_BUILD_NUMBER', ''))
+
+
+def invoke(name, payload):
+    """
+    invokes a lambda
+    :param name: the name of the lambda to invoke
+    :param payload: the payload
+    :return: the response
+    """
+    lambda_client = boto3.client('lambda', region_name='us-east-1')
+    return lambda_client.invoke(
+        FunctionName='{}-{}'.format(SERVICE_PREFIX, name),
+        InvocationType='RequestResponse',
+        Payload=payload
+    )
+
+
+class TestLambdaWrapper:
+    @pytest.mark.parametrize("input", [
+        '',
+        '{afwe',
+        [],
+        [1, 2, 3],
+        {},
+        {'test': 'test'},
+        {'test': 'test', 'more': [1, 2, '3']},
+    ])
+    def test_sanity_valid_input(self, input):
+        response = invoke('sanity', json.dumps(input))
+        assert response['StatusCode'] == 200
+        content = json.loads(response['Payload'].read())
+        assert content['statusCode'] == 200
+        body = json.loads(content['body'])
+        assert body['input'] == input
+
+    @pytest.mark.parametrize("input", [
+        '',
+        '{afwe',
+        [],
+        [1, 2, 3],
+        {},
+        {'test': 'test'},
+        {'test': 'test', 'more': [1, 2, '3']},
+    ])
+    def test_labels(self, input):
+        response = invoke('labels', json.dumps(input))
+        assert response['StatusCode'] == 200
+        content = json.loads(response['Payload'].read())
+        assert content['statusCode'] == 200
+        body = json.loads(content['body'])
+        assert body['input'] == input
+
+
+
--- acceptance/run.sh
+++ acceptance/run.sh
@@ -2,19 +2,18 @@
 cd acceptance/lambda-handlers
 
 build_num=$1
-
+result=0
 function run_acceptance_test() {
     runtime=$1
     echo "deploying of ${runtime} [build: ${build_num}]"
-    serverless deploy --runtime ${runtime} --buildNumber ${build_num} || {  echo "deployment of ${runtime} [build: ${build_num}] failed" ; exit 1; }
-    serverless invoke -f sanity --runtime ${runtime} --buildNumber ${build_num}  || {  echo "test ${runtime} [build: ${build_num}] failed" ; serverless remove --runtime ${runtime}  --buildNumber ${build_num} ; exit 1; }
-    serverless invoke -f sanity --runtime ${runtime} --buildNumber ${build_num} -p bad_events.json || {  echo "test ${runtime} [build: ${build_num}] with bad event arguments failed" ; serverless remove --runtime ${runtime}  --buildNumber ${build_num} ; exit 1; }
-    serverless invoke -f labels --runtime ${runtime} --buildNumber ${build_num} || {  echo "test ${runtime} [build: ${build_num}] labels failed" ; serverless remove --runtime ${runtime}  --buildNumber ${build_num} ; exit 1; }
+    serverless deploy --runtime ${runtime} --buildNumber ${build_num} || {  echo "deployment of ${runtime} [build: ${build_num}] failed" ; result=1; }
+    pytest ../acceptance.py || {  echo "tests ${runtime} [build: ${build_num}] failed" ; result=1; }
     serverless remove --runtime ${runtime} --buildNumber ${build_num}
 }
 
-run_acceptance_test py36
-run_acceptance_test py27
+run_acceptance_test python3.6
+#run_acceptance_test python3.7
+run_acceptance_test python2.7
 
 cd -
-exit 0
+exit ${result}
