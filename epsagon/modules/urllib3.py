ae1f0d2582de5cf95380cc5e2bd6366c1de08240
Erez Freiberger <erez@epsagon.com>
2019-05-23 18:53:08 +0300

    Urllib3 support (#87)
    * fix(tracer): on EPSAGON_DEBUG print trace even if failed to send due to timeout
    
    * fix(requests): set requests resource_type to http
    
    * feature(urllib3): add urllib3 instrumentation
    
    * removing requests instrumentation
    
    removing requests instrumentation patching.
    
    Not needed as we now instrument urllib3 and it duplicates
    
    requests is using urllib3 (since forever basically) so it covers all the
    reasonable cases

---
+++ epsagon/modules/urllib3.py
@@ -0,0 +1,42 @@
+"""
+urllib3 patcher module.
+"""
+
+from __future__ import absolute_import
+import wrapt
+from epsagon.modules.general_wrapper import wrapper
+from ..events.urllib3 import Urllib3EventFactory
+
+
+def _wrapper(wrapped, instance, args, kwargs):
+    """
+    General wrapper for requests instrumentation.
+    :param wrapped: wrapt's wrapped
+    :param instance: wrapt's instance
+    :param args: wrapt's args
+    :param kwargs: wrapt's kwargs
+    :return: None
+    """
+    return wrapper(Urllib3EventFactory, wrapped, instance, args, kwargs)
+
+
+def patch():
+    """
+    Patch module.
+    :return: None
+    """
+
+    try:
+        wrapt.wrap_function_wrapper(
+            'urllib3',
+            'HTTPConnectionPool.urlopen',
+            _wrapper
+        )
+        wrapt.wrap_function_wrapper(
+            'urllib3',
+            'HTTPSConnectionPool.urlopen',
+            _wrapper
+        )
+    except Exception:  # pylint: disable=broad-except
+        # Can happen in different Python versions.
+        pass
