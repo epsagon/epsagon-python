69a3cd03b36c05ca64d9150efd38d6e00f32d68b
Tal Vintrob <tal@epsagon.com>
2019-08-29 14:56:13 +0300

    fix: pr comments fixes

--- epsagon/trace_transports.py
+++ epsagon/trace_transports.py
@@ -21,8 +21,8 @@ class NoneTransport(object):
 class LogTransport(object):
     """ send traces by logging them """
 
-    @classmethod
-    def send(cls, trace):
+    @staticmethod
+    def send(trace):
         trace_json = to_json(trace.to_dict())
         trace_message = base64.b64encode(
             trace_json.encode('utf-8')
