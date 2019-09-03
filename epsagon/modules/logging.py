421b00c2cdd247c94c421e45cdaa0fe577884f76
razlo <raz@epsagon.com>
2019-09-03 15:38:42 +0300

    fix(logging): Make logging flag cap insensitive (#143)

--- epsagon/modules/logging.py
+++ epsagon/modules/logging.py
@@ -17,7 +17,9 @@ def _wrapper(wrapped, _instance, args, kwargs):
     :param kwargs: wrapt's kwargs
     :return: None
     """
-    if os.getenv('EPSAGON_DISABLE_LOGGING_ERRORS') != 'TRUE':
+    if (os.getenv(
+            'EPSAGON_DISABLE_LOGGING_ERRORS'
+    ) or '').upper() == 'TRUE':
         trace_factory.set_error(*args)
     return wrapped(*args, **kwargs)
 
