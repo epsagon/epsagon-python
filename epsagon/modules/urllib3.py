912979cb43924f55c9707be50783837f7fe96ca6
razlo <raz@epsagon.com>
2019-11-03 15:36:00 +0200

    feat(urllib3): filter blacklisted urls for http header injections

--- epsagon/modules/urllib3.py
+++ epsagon/modules/urllib3.py
@@ -3,9 +3,11 @@ urllib3 patcher module.
 """
 
 from __future__ import absolute_import
+import uuid
 import wrapt
 from epsagon.modules.general_wrapper import wrapper
 from ..events.urllib3 import Urllib3EventFactory
+from ..http_filters import is_blacklisted_url
 
 
 def _wrapper(wrapped, instance, args, kwargs):
@@ -17,6 +19,24 @@ def _wrapper(wrapped, instance, args, kwargs):
     :param kwargs: wrapt's kwargs
     :return: None
     """
+    # Inject header to support tracing over HTTP requests to
+    # opentracing monitored code
+    trace_id = uuid.uuid4().hex
+    span_id = uuid.uuid4().hex[16:]
+    parent_span_id = uuid.uuid4().hex[16:]
+
+    host_url = '{}://{}'.format(instance.scheme, instance.host)
+
+    # Detect if URL is blacklisted, and ignore.
+    if not is_blacklisted_url(host_url):
+        kwargs['headers']['epsagon-trace-id'] = (
+            '{trace_id}:{span_id}:{parent_span_id}:1'.format(
+                trace_id=trace_id,
+                span_id=span_id,
+                parent_span_id=parent_span_id
+            )
+        )
+
     return wrapper(Urllib3EventFactory, wrapped, instance, args, kwargs)
 
 
