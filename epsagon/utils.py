ca6a5917ac2fe6766be94bdec06373215e1d052b
idonava <idonava@gmail.com>
2020-03-01 12:56:59 +0200

    feat(stepfunctions): Improve aws stepfunctions support (#202)
    feat(stepfunctions): add support to sendTaskSuccess, SendTaskHeartbeat, DescribeExecution, SQS lambda trigger

--- epsagon/utils.py
+++ epsagon/utils.py
@@ -8,6 +8,7 @@ import collections
 import socket
 import sys
 import traceback
+import re
 import six
 import requests
 import simplejson as json
@@ -250,6 +251,7 @@ def find_in_object(obj, key):
 
     return result
 
+
 def collect_exception_python3(exception):
     """
     Collect exception from exception __traceback__.
@@ -264,6 +266,7 @@ def collect_exception_python3(exception):
     ))
     return traceback_data
 
+
 def collect_exception_python2():
     """
     Collect exception from exception sys.exc_info.
@@ -273,6 +276,7 @@ def collect_exception_python2():
     traceback_data = six.StringIO()
     traceback.print_exception(*sys.exc_info(), file=traceback_data)
     return traceback_data.getvalue()
+
 
 def get_traceback_data_from_exception(exception):
     """
@@ -286,3 +290,44 @@ def get_traceback_data_from_exception(exception):
     if python_version == 3:
         return collect_exception_python3(exception)
     return ''
+
+
+def parse_json(json_string):
+    """
+    Parse JSON string to a Python Dictionary
+    :param json_string: JSON string
+    :return: Python Dictionary
+    """
+    try:
+        return json.loads(json_string)
+    except ValueError:
+        return None
+
+
+def camel_case_to_title_case(camel_case_string):
+    """
+    Turn Camel Case string into Title Case string in which first characters of
+    all the words are capitalized.
+    :param camel_case_string: Camel Case string
+    :return: Title Case string
+    """
+    if not isinstance(camel_case_string, str):
+        return None
+    title_case = re.sub('([^-])([A-Z][a-z-]+)', r'\1 \2', camel_case_string)\
+        .title()
+    return title_case
+
+
+def add_metadata_from_dict(resource, dictionary, key):
+    """
+    Add new data to resource metadata
+    :param resource: Resource
+    :param dictionary: Argument Dictionary
+    :param key: Property key
+    :return: True if added, else False
+    """
+    value = dictionary.get(key)
+    if not value and not isinstance(value, str):
+        return
+    title_case_key = camel_case_to_title_case(key)
+    resource['metadata'][title_case_key] = value
