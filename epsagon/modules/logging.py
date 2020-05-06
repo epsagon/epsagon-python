9cd1a977d11325a18e390445163a7c592870b83e
ranrib <ran@epsagon.com>
2020-05-06 12:17:05 +0300

    fix(logging): update logging instrumentation behaviour

--- epsagon/modules/logging.py
+++ epsagon/modules/logging.py
@@ -5,8 +5,18 @@ logging patcher module.
 from __future__ import absolute_import
 from functools import partial
 import os
+import json
 import wrapt
 from ..trace import trace_factory
+
+LOGGING_FUNCTIONS = (
+    'info',
+    'debug',
+    'error',
+    'warning',
+    'exception',
+    'critical'
+)
 
 
 def _wrapper(wrapped, _instance, args, kwargs):
@@ -25,16 +35,11 @@ def _wrapper(wrapped, _instance, args, kwargs):
     return wrapped(*args, **kwargs)
 
 
-def _get_log_id():
-    """
-    retrieve or generate the log id for this trace
-    """
-    return trace_factory.get_log_id()
-
-
 def _epsagon_trace_id_wrapper(msg_index, wrapped, _instance, args, kwargs):
     """
     Wrapper for logging module.
+    :param msg_index: the index of the log message in args
+        (since Logger.log also gets `level`)
     :param wrapped: wrapt's wrapped
     :param _instance: wrapt's instance, unused.
     :param args: wrapt's args
@@ -42,8 +47,20 @@ def _epsagon_trace_id_wrapper(msg_index, wrapped, _instance, args, kwargs):
     :return: None
     """
     if trace_factory.is_logging_tracing_enabled():
-        msg = ' '.join([_get_log_id(), args[msg_index]])
-        args = args[0:msg_index] + (msg,) + args[(msg_index+1):]
+        trace_log_id = trace_factory.get_log_id()
+        try:
+            # Check if message is in json format
+            json_log = json.loads(args[msg_index])
+            json_log['epsagon'] = {'trace_id': trace_log_id}
+            message = json.dumps(json_log)
+        except Exception:  # pylint: disable=broad-except
+            # message is a regular string, add the ID to the beginning
+            message = ' '.join([trace_log_id, args[msg_index]])
+        args = (
+            args[0:msg_index] +
+            (message,) +
+            args[(msg_index + 1):]
+        )
     return wrapped(*args, **kwargs)
 
 
@@ -52,14 +69,26 @@ def patch():
     Patch module.
     :return: None
     """
+    # Automatically capture exceptions from logging
     wrapt.wrap_function_wrapper('logging', 'exception', _wrapper)
     wrapt.wrap_function_wrapper('logging', 'Logger.exception', _wrapper)
+
+    # Instrument logging with Epsagon trace ID
     wrapt.wrap_function_wrapper(
-        'logging', 'Logger.log', partial(_epsagon_trace_id_wrapper, 1))
-    # wrapt.wrap_function_wrapper(
-    #     'builtins', 'print', partial(_epsagon_trace_id_wrapper, 0))
-    for log_function in ['info', 'debug', 'error',
-                         'warning', 'exception', 'critical']:
+        'logging',
+        'Logger.log',
+        partial(_epsagon_trace_id_wrapper, 1)
+    )
+    for log_function in LOGGING_FUNCTIONS:
         wrapt.wrap_function_wrapper(
-            'logging', 'Logger.' + log_function,
-            partial(_epsagon_trace_id_wrapper, 0))
+            'logging',
+            'Logger.{}'.format(log_function),
+            partial(_epsagon_trace_id_wrapper, 0)
+        )
+
+    # Instrument print function is disabled
+    # wrapt.wrap_function_wrapper(
+    #   'builtins',
+    #   'print',
+    #   partial(_epsagon_trace_id_wrapper, 0)
+    # )
--- tests/test_trace.py
+++ tests/test_trace.py
@@ -874,7 +874,7 @@ def test_init_sanity(wrapped_init, _create):
         transport=default_http,
         split_on_send=False,
         propagate_lambda_id=False,
-        logging_tracing_enabled=False,
+        logging_tracing_enabled=True,
     )
 
 
@@ -902,7 +902,7 @@ def test_init_empty_app_name(wrapped_init, _create):
         transport=default_http,
         split_on_send=False,
         propagate_lambda_id=False,
-        logging_tracing_enabled=False,
+        logging_tracing_enabled=True,
     )
 
 
@@ -924,7 +924,7 @@ def test_init_empty_collector_url(wrapped_init, _create):
         transport=default_http,
         split_on_send=False,
         propagate_lambda_id=False,
-        logging_tracing_enabled=False,
+        logging_tracing_enabled=True,
     )
 
 
@@ -950,7 +950,7 @@ def test_init_no_ssl_no_url(wrapped_init, _create):
         transport=default_http,
         split_on_send=False,
         propagate_lambda_id=False,
-        logging_tracing_enabled=False,
+        logging_tracing_enabled=True,
     )
 
 
@@ -980,7 +980,7 @@ def test_init_ssl_no_url(wrapped_init, _create):
         transport=default_http,
         split_on_send=False,
         propagate_lambda_id=False,
-        logging_tracing_enabled=False,
+        logging_tracing_enabled=True,
     )
 
 
@@ -1008,7 +1008,7 @@ def test_init_ssl_with_url(wrapped_init, _create):
         transport=default_http,
         split_on_send=False,
         propagate_lambda_id=False,
-        logging_tracing_enabled=False,
+        logging_tracing_enabled=True,
     )
 
 
@@ -1036,7 +1036,7 @@ def test_init_no_ssl_with_url(wrapped_init, _create):
         transport=default_http,
         split_on_send=False,
         propagate_lambda_id=False,
-        logging_tracing_enabled=False,
+        logging_tracing_enabled=True,
     )
 
 
@@ -1064,7 +1064,7 @@ def test_init_ignored_urls_env(wrapped_init, _create):
         transport=default_http,
         split_on_send=False,
         propagate_lambda_id=False,
-        logging_tracing_enabled=False,
+        logging_tracing_enabled=True,
     )
     os.environ.pop('EPSAGON_URLS_TO_IGNORE')
 
@@ -1093,7 +1093,7 @@ def test_init_keys_to_ignore(wrapped_init, _create):
         transport=default_http,
         split_on_send=False,
         propagate_lambda_id=False,
-        logging_tracing_enabled=False,
+        logging_tracing_enabled=True,
     )
 
 
@@ -1122,7 +1122,7 @@ def test_init_keys_to_ignore_env(wrapped_init, _create):
         transport=default_http,
         split_on_send=False,
         propagate_lambda_id=False,
-        logging_tracing_enabled=False,
+        logging_tracing_enabled=True,
     )
     os.environ.pop('EPSAGON_IGNORED_KEYS')
 
@@ -1151,7 +1151,7 @@ def test_init_split_on_send(wrapped_init, _create):
         keys_to_allow=None,
         split_on_send=True,
         propagate_lambda_id=False,
-        logging_tracing_enabled=False,
+        logging_tracing_enabled=True,
     )
 
 
@@ -1179,9 +1179,38 @@ def test_init_split_on_send_env(wrapped_init, _create):
         keys_to_allow=None,
         split_on_send=True,
         propagate_lambda_id=False,
-        logging_tracing_enabled=False,
+        logging_tracing_enabled=True,
     )
     os.environ.pop('EPSAGON_SPLIT_ON_SEND')
+
+
+@mock.patch('epsagon.utils.create_transport', side_effect=lambda x, y: default_http)
+@mock.patch('epsagon.trace.TraceFactory.initialize')
+def test_init_logging_disabled_on_lambda(wrapped_init, _create):
+    os.environ['AWS_LAMBDA_FUNCTION_NAME'] = 'func-name'
+    epsagon.utils.init(
+        token='token',
+        app_name='app-name',
+        collector_url="http://abc.com",
+        metadata_only=False,
+    )
+    wrapped_init.assert_called_with(
+        token='token',
+        app_name='app-name',
+        metadata_only=False,
+        collector_url="http://abc.com",
+        disable_timeout_send=False,
+        debug=False,
+        send_trace_only_on_error=False,
+        url_patterns_to_ignore=None,
+        transport=default_http,
+        keys_to_ignore=None,
+        keys_to_allow=None,
+        split_on_send=False,
+        propagate_lambda_id=False,
+        logging_tracing_enabled=False,
+    )
+    os.environ.pop('AWS_LAMBDA_FUNCTION_NAME')
 
 
 @mock.patch('requests.Session.post', side_effect=requests.ReadTimeout)
@@ -1328,7 +1357,7 @@ def test_init_propagate_lambda_identifier_env(wrapped_init, _create):
         keys_to_allow=None,
         split_on_send=False,
         propagate_lambda_id=True,
-        logging_tracing_enabled=False,
+        logging_tracing_enabled=True,
     )
     os.environ.pop('EPSAGON_PROPAGATE_LAMBDA_ID')
 
@@ -1357,5 +1386,5 @@ def test_init_propagate_lambda_identifier_init(wrapped_init, _create):
         keys_to_allow=None,
         split_on_send=False,
         propagate_lambda_id=True,
-        logging_tracing_enabled=False,
+        logging_tracing_enabled=True,
     )
