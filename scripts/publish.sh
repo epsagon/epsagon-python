c277d08d86ea63913f6bd92dadb11dadb4db33b6
maorlx <59009317+maorlx@users.noreply.github.com>
2020-05-10 15:51:19 +0300

    fix(layer): moving sleep to the correct place (#231)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "releasing new version..." &&
 ./scripts/semantic_release.sh &&
-sleep 1 &&
+sleep 60 &&
 echo "publishing layer..." &&
 ./scripts/publish_layer.sh &&
 echo "deployment successful"
