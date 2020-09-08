b81d7a47eee9983b47968b0a6b8f5d752c7933e4
ranrib <ran@epsagon.com>
2020-09-09 00:26:09 +0300

    fix(tests)

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 # Skip aio based files from older Python versions
 ret=`python -c 'import sys; print(0 if sys.version_info < (3, 5, 3) else 1)'`
-echo $ret
 excludes=''
 if [ $ret -eq 0 ]; then
-    excludes='aiohttp'
+    pytest --ignore-glob=*aiohttp*
+else
+    pytest
 fi
-echo $excludes
-pytest --ignore-glob=*$excludes*
