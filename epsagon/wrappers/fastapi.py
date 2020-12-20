4a02cf857202a5d5e846aadfd0bf0d34da024950
maorlx <59009317+maorlx@users.noreply.github.com>
2020-12-20 15:43:23 +0200

    fix(fastapi): fast api errors handling (#308)

--- epsagon/wrappers/fastapi.py
+++ epsagon/wrappers/fastapi.py
@@ -18,7 +18,7 @@ from epsagon.utils import (
     get_traceback_data_from_exception
 )
 from ..http_filters import ignore_request
-
+from ..utils import print_debug
 
 class TracingAPIRoute(APIRoute):
     """
@@ -35,12 +35,20 @@ class TracingAPIRoute(APIRoute):
             Traces given request and its response.
             :param request: to trace
             """
-            epsagon.trace.trace_factory.switch_to_async_tracer()
+            should_ignore_request = True
+            try:
+                epsagon.trace.trace_factory.switch_to_async_tracer()
+                if not ignore_request('', request.url.path.lower()):
+                    should_ignore_request = False
+                    trace = epsagon.trace.trace_factory.get_or_create_trace()
+                    trace.prepare()
 
-            if ignore_request('', request.url.path.lower()):
+            except Exception as exception: # pylint: disable=W0703
                 return await original_route_handler(request)
-            trace = epsagon.trace.trace_factory.get_or_create_trace()
-            trace.prepare()
+
+            if should_ignore_request:
+                return await original_route_handler(request)
+
             runner = None
             response = None
             try:
@@ -53,24 +61,31 @@ class TracingAPIRoute(APIRoute):
                 collect_container_metadata(runner.resource['metadata'])
             except Exception as exception: # pylint: disable=W0703
                 warnings.warn('Could not extract request', EpsagonWarning)
-
+            raised_err = None
             try:
                 response: Response = await original_route_handler(request)
             except Exception as exception:  # pylint: disable=W0703
+                raised_err = exception
                 traceback_data = get_traceback_data_from_exception(exception)
                 trace.runner.set_exception(exception, traceback_data)
 
-            if response is not None and runner:
-                if ignore_request(
-                        response.headers.get('Content-Type', '').lower(),
-                        ''
-                ):
-                    return response
+            try:
+                if not raised_err and response is not None and runner:
+                    if ignore_request(
+                            response.headers.get('Content-Type', '').lower(),
+                            ''
+                    ):
+                        return response
 
-                runner.update_response(response)
+                    runner.update_response(response)
 
-            if runner:
-                epsagon.trace.trace_factory.send_traces()
+                if runner:
+                    epsagon.trace.trace_factory.send_traces()
+            except Exception as exception:  # pylint: disable=W0703
+                print_debug('Failed to send traces: {}'.format(exception))
+
+            if raised_err:
+                raise raised_err
 
             return response
 
