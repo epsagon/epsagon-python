895d001c0c743297d0a46d439500d900d3aebc65
Maor Levi <maor@epsagon.com>
2021-05-23 14:50:24 +0300

    Adding custom api routes support

--- epsagon/modules/fastapi.py
+++ epsagon/modules/fastapi.py
@@ -4,35 +4,13 @@ fastapi patcher module.
 
 from __future__ import absolute_import
 import wrapt
-from fastapi.routing import APIRoute
 from ..wrappers.fastapi import (
-    TracingAPIRoute,
     exception_handler_wrapper,
     server_call_wrapper,
+    route_class_wrapper,
 )
-from ..utils import is_lambda_env, print_debug
+from ..utils import is_lambda_env
 
-def _wrapper(wrapped, _instance, args, kwargs):
-    """
-    Adds TracingRoute into APIRouter (FastAPI).
-    :param wrapped: wrapt's wrapped
-    :param instance: wrapt's instance
-    :param args: wrapt's args
-    :param kwargs: wrapt's kwargs
-    """
-
-    # Skip on Lambda environment since it's not relevant and might be duplicate
-    if is_lambda_env():
-        return wrapped(*args, **kwargs)
-    route_class = kwargs.get('route_class', APIRoute)
-    if route_class != APIRoute:
-        # custom routes are not supported
-        print_debug(
-            f'Custom FastAPI route {route_class.__name__} is not supported'
-        )
-        return wrapped(*args, **kwargs)
-    kwargs['route_class'] = TracingAPIRoute
-    return wrapped(*args, **kwargs)
 
 def _exception_handler_wrapper(wrapped, _instance, args, kwargs):
     """
@@ -42,10 +20,6 @@ def _exception_handler_wrapper(wrapped, _instance, args, kwargs):
     :param args: wrapt's args
     :param kwargs: wrapt's kwargs
     """
-    # Skip on Lambda environment since it's not relevant and might be duplicate
-    if is_lambda_env():
-        return wrapped(*args, **kwargs)
-
     if args and len(args) == 2:
         args = list(args)
         args[1] = exception_handler_wrapper(args[1])
@@ -57,10 +31,14 @@ def patch():
     Patch module.
     :return: None
     """
+    # Skip on Lambda environment since it's not relevant and might be duplicate
+    if is_lambda_env():
+        return
+
     wrapt.wrap_function_wrapper(
-        'fastapi',
-        'APIRouter.__init__',
-        _wrapper
+        'fastapi.routing',
+        'APIRoute.__init__',
+        route_class_wrapper
     )
     wrapt.wrap_function_wrapper(
         'starlette.applications',
