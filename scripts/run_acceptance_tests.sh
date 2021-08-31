da65c18098be1476488880291f282dd2604b5c4a
Ophir Yael <ofir.yael@gmail.com>
2021-08-31 12:07:32 +0300

    Add acceptance tests script

---
+++ scripts/run_acceptance_tests.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+if [ -z $AWS_ACCESS_KEY_ID ] || [ -z $AWS_SECRET_ACCESS_KEY ]; then
+    echo "AWS credentials must be set in order to run acceptance tests"
+    exit 1
+else
+    npm install && export PATH=$(pwd)/node_modules/.bin:$PATH
+    ../acceptance/run.sh $TRAVIS_BUILD_NUMBER $TRAVIS_PYTHON_VERSION
+fi
