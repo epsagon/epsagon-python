1ea7e4cb8d6d30e81cc78eaddf6acbc4693f1658
Maor Levi <maor@epsagon.com>
2021-09-13 14:12:21 +0200

    Fixing error setting

--- epsagon/modules/django.py
+++ epsagon/modules/django.py
@@ -87,6 +87,8 @@ def gunicorn_sync_wrapper(wrapped, _instance, args, kwargs):
         pass
     try:
         return wrapped(*args, **kwargs)
+    except StopIteration:
+        pass
     except Exception as error: # pylint: disable=broad-except
         trace_factory.set_error(error, traceback.format_exc())
         raise error
