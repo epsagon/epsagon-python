7aaf7321e9c81fb21078d882077e6cb04abf0d7d
sagivr2020 <sagivram15193@gmail.com>
2022-01-02 13:16:09 +0200

    fix(pymongo): extend support for more operations

--- epsagon/events/pymongo.py
+++ epsagon/events/pymongo.py
@@ -19,6 +19,8 @@ class PyMongoEvent(BaseEvent):
 
     ORIGIN = 'pymongo'
     RESOURCE_TYPE = 'pymongo'
+    INSERT_OPERATIONS = ['insert_one', 'insert_many']
+    FILTER_OPERATIONS = ['find', 'update_one', 'delete_many']
 
     #pylint: disable=W0613
     def __init__(self, wrapped, instance, args, kwargs, start_time, response,
@@ -33,30 +35,37 @@ class PyMongoEvent(BaseEvent):
         :param response: response data
         :param exception: Exception (if happened)
         """
-
         super(PyMongoEvent, self).__init__(start_time)
 
-        documents = args[0]
+        self.resource['operation'] = getattr(wrapped, '__name__')
+
+        if len(args) == 0:
+            documents = list()
+
+        else:
+            documents = args[0]
+
+        # print('docs: ', [str(x.document) for x in documents])
 
         self.event_id = 'mongo-{}'.format(str(uuid4()))
-        self.resource['name'] = instance.full_name
-
-        self.resource['operation'] = \
-            'insert_many' if isinstance(documents, list) else 'insert_one'
+        
+        self.resource['name'] = instance.name
         address = list(getattr(
             instance.database.client,
             '_topology_settings'
-        ).seeds)[0]
-
-        if self.resource['operation'] == 'insert_one':
-            documents = [documents]
+            ).seeds)[0]
 
         self.resource['metadata'] = {
-            'DB URL': ':'.join([str(x) for x in address]),
-            'DB Name': str(instance.database.name),
-            'Collection Name': str(instance.name),
-        }
-        add_data_if_needed(self.resource['metadata'], 'Items', documents)
+                'DB URL': ':'.join([str(x) for x in address]),
+                'DB Name': str(instance.database.name),
+                'Collection Name': str(instance.collection.name),
+            }
+
+        if self.resource['operation'] in PyMongoEvent.INSERT_OPERATIONS:
+            add_data_if_needed(self.resource['metadata'], 'Items', documents)
+        
+        elif self.resource['operation'] in PyMongoEvent.FILTER_OPERATIONS:
+            add_data_if_needed(self.resource['metadata'], 'Search', documents)
 
         if response is not None:
             self.update_response(response)
@@ -70,17 +79,38 @@ class PyMongoEvent(BaseEvent):
         :param response: Response from botocore
         :return: None
         """
-        for i in range(len(self.resource['metadata'].get('items', []))):
-            self.resource['metadata']['items'][i]['_id'] = str(
-                self.resource['metadata']['items'][i]['_id']
-            )
 
-        if self.resource['operation'] == 'insert_many':
-            self.resource['metadata']['inserted_ids'] = \
-                [str(x) for x in response.inserted_ids]
-        elif self.resource['operation'] == 'insert_one':
-            self.resource['metadata']['inserted_ids'] = \
-                [str(response.inserted_id)]
+        if self.resource['operation'] in PyMongoEvent.INSERT_OPERATIONS:
+            for i in range(len(self.resource['metadata'].get('items', []))):
+                self.resource['metadata']['items'][i]['_id'] = str(
+                    self.resource['metadata']['items'][i]['_id']
+                )
+
+            if self.resource['operation'] == 'insert_many':
+                self.resource['metadata']['inserted_ids'] = \
+                    [str(x) for x in response.inserted_ids]
+            elif self.resource['operation'] == 'insert_one':
+                self.resource['metadata']['inserted_ids'] = \
+                    [str(response.inserted_id)]
+
+        elif self.resource['operation'] in PyMongoEvent.FILTER_OPERATIONS:
+            self.resource['metadata']['Search'] = str(
+                    self.resource['metadata']['Search']
+                )
+            
+            if self.resource['operation'] == 'find':
+                self.resource['metadata']['Results'] = \
+                        [x for x in response]
+        
+            elif self.resource['operation'] in ['update_one']:
+                self.resource['metadata']['matched_count'] = \
+                        response.matched_count
+                self.resource['metadata']['modified_count'] = \
+                        response.modified_count
+        
+            elif self.resource['operation'] in ['delete_many']:  
+                self.resource['metadata']['deleted_count'] = \
+                        response.deleted_count
 
 
 class PyMongoEventFactory(object):
